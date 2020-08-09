58672936a493e28624d3a739f43fbec34e64ebef
cycvez <cycvez@gmail.com>
2020-08-08 23:40:20 -0500

    new problem

--- src/main/java/com/mycompany/playground/NewYearsChaos.java
+++ src/main/java/com/mycompany/playground/NewYearsChaos.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.playground;
 
+import java.util.Arrays;
+
 /**
  *
  * @author carlo
@@ -12,11 +14,22 @@ package com.mycompany.playground;
 public class NewYearsChaos {
 
     static void minimumBribes(int[] q) {
-
+        int size = q.length;
+        int[] original = new int[size];
+        int[] temp= new int [size];
+        int count=0;
+        for (int i = 0; i < size; i++) {
+            original[i] = i + 1;
+        }
+        while(original!=q){
+            
+        }
+        System.out.println(Arrays.toString(original));
     }
-    
+
     public static void main(String[] args) {
-        
+        int[] q = {5, 3, 2, 1, 4};
+        minimumBribes(q);
     }
 
 }
